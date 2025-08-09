1589144bace645a205123ab245404082666b71b8
ab-yf <abdullahyusuf.bmiib@gmail.com>
2025-08-10 00:18:38 +0300

    feat: display movies, add loading state and error handling to movie fetching

--- react-2hr-cc/src/App.jsx
+++ react-2hr-cc/src/App.jsx
@@ -17,9 +17,12 @@ const App = () => {
     const [errorMessage, setErrorMessage] = useState('')
     const [movieList, setMovieList] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
-    
+
 
     const fetchMovies = async () => {
+        setIsLoading(true);
+        setErrorMessage('');
+
         try {
             const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
             const response = await fetch(endpoint, API_OPTIONS)
@@ -29,12 +32,17 @@ const App = () => {
             const data = await response.json();
             if (data.Response === 'False') {
                 setErrorMessage(data.Error || 'Failed to fetch movies');
+                setMovieList([]);
+                return;
             }
 
+            setMovieList(data.results || []);
 
         } catch (error) {
             console.log(`Error Fetching Movies: ${error}`);
             setErrorMessage('Error fetching movies, please try again later.');
+        } finally {
+            setIsLoading(false);
         }
     }
 
@@ -52,9 +60,15 @@ const App = () => {
             </header>
             <section className="all-movies">
                 <h2>All Movies</h2>
-
-                {errorMessage && <p className="text-red-500">{errorMessage}</p>}
-
+                {isLoading ? (
+                    <p className="text-white">Loading...</p>
+                ) : errorMessage ? (
+                    <p className="text-red-500">{errorMessage}</p>
+                ) : (
+                    <ul>
+                        {movieList.map((movie) => (<p className="text-white">{movie.title}</p>))}
+                    </ul>
+                )}
             </section>
         </div>
     </main>)
