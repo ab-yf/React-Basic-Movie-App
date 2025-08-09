a4a1bcab6e798c53e417a0d726ee30a8fae3d2d8
ab-yf <abdullahyusuf.bmiib@gmail.com>
2025-08-10 00:44:42 +0300

    feat: enhance MovieCard component to display release date and improve layout

--- react-2hr-cc/src/components/MovieCard.jsx
+++ react-2hr-cc/src/components/MovieCard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const MovieCard = ({
-  movie: { title, vote_average, poster_path, original_language },
+  movie: { title, vote_average, poster_path, release_date, original_language },
 }) => {
   return (
     <div className="movie-card">
@@ -11,7 +11,23 @@ const MovieCard = ({
             ? `https://image.tmdb.org/t/p/w500/${poster_path}`
             : "/no-movie.png"
         }
+        alt={title}
       />
+      <div className="mt-4">
+        <h3>{title}</h3>
+        <div className="content">
+          <div className="rating">
+            <img src="star.svg" alt="Star Icon" />
+            <p>{vote_average ? vote_average.toFixed(1) : "N/A"}</p>
+          </div>
+          <span>•</span>
+          <p className="lang">{original_language}</p>
+          <span>•</span>
+          <p className="year">
+            {release_date ? release_date.split("-")[0] : "N/A"}
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
